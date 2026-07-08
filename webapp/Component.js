7091f3449aada9b888cba7a488f269de955b7df3
bobbybtp <bobby.sapui5@gmail.com>
2026-07-08 02:38:55 +0000

    firstcommit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zsd85.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
